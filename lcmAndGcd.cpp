d0679cc3faf8ffd22767b74537b9e17545c4b11f
@Vishal-Telukula <telukulasunitha@gmail.com>
2024-12-28 17:56:22 +0530

    commiting checkPalindrome

---
+++ lcmAndGcd.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> lcmAndGcd(int a, int b) {
+        int gcd;
+        for(int i=1; i< max(a,b);i++){
+            if(a % i == 0 && b % i == 0){
+                gcd = i;
+            }
+        }
+        int lcm=a*b/gcd;
+
+        return {lcm,gcd};
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int A, B;
+
+        cin >> A >> B;
+
+        Solution ob;
+        vector<int> ans = ob.lcmAndGcd(A, B);
+        cout << ans[0] << " " << ans[1] << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
