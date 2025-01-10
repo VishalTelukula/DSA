3b23e5908f38427b8bc45e3665f8e5a627edb85a
@Vishal-Telukula <telukulasunitha@gmail.com>
2025-01-10 10:40:46 +0530

    --

---
+++ 4Sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>> sst;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                set<long long > hashset;
+                for(int k=j+1;k<n;k++){
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long long fourth = target - (sum);
+                    if(hashset.find(fourth) != hashset.end()){
+                        vector<int> temp = {nums[i],nums[j],nums[k],(int)(fourth)};
+                        sort(temp.begin(),temp.end());
+                        sst.insert(temp);
+                    }
+                    hashset.insert(nums[k]);
+                }
+            }
+        }
+        vector<vector<int>> res(sst.begin(),sst.end());
+        return res;
+    }
+};
